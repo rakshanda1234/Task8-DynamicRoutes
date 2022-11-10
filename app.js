495b1cf8d182851c599cdf5df5147a48af152c0a
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-10 15:31:42 +0530

    Task17-sequelize deliverable update delete and get product

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -22,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000);
