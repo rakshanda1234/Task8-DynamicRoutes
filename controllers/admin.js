329cde39f2a19246ebf291f8b77218dca961c638
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-20 19:24:11 +0530

    Task21-one to many relationship

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  req.user.createProduct();
+
   Product.create({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description,
+    userId: req.user.id,
   })
     .then((result) => {
       //console.log(result);
@@ -35,8 +38,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
     .then((product) => {
+      const products = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -75,13 +81,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  });
+  req.user
+    .getProducts()
+    // Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
